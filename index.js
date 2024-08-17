e245af24afcbd3eb3e8ba881ecc086f0ed76c1ac
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-17 19:15:29 +0600

    module

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const greeting = require("./greet.js");
-
-greeting("mafi");
+const { people1, people2, people3 } = require("./people.js");
+greeting("nahidparvezmafi");
+greeting(people1);
+greeting(people2);
+greeting(people3);
---
+++ people.js
@@ -0,0 +1,5 @@
+const people1 = "Nahid";
+const people2 = "Parvez";
+const people3 = "Mafi";
+
+module.exports = { people1, people2, people3 };
